e1a18ba4fc68d0233293d2f45daa87be9f9cfcb5
JavierPeron <peronpapa17@gmail.com>
2022-09-03 11:36:27 -0300

    a ver2

--- proyectofontana/settings/base.py
+++ proyectofontana/settings/base.py
@@ -128,9 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "Fund-Fontana/static"),),
+STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), 'static')),
 MEDIA= "/media/"
-MEDIA_ROOT = (os.path.join(BASE_DIR, "Fund-Fontana/media"),),
+MEDIA_ROOT = (os.path.join(os.path.dirname(BASE_DIR), 'media'),),
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
