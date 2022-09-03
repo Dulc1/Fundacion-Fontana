207beec2c0dcb3713c0705195eb53f48f3c980ee
JavierPeron <peronpapa17@gmail.com>
2022-09-03 01:47:54 -0300

    herukofinal

---
+++ proyectofontana/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for proyectofontana project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectofontana.settings.production')
+
+application = get_wsgi_application()
+application = WhiteNoise(application)
